dfe692e30971bf55e68f7bff04e27274b8f14a6c
Alex Lee <3076438032@qq.com>
2026-01-14 22:24:14 +0800

    feat: inngest agent test

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,27 @@
 import { inngest } from "./client";
+import { createAgent, openai } from '@inngest/agent-kit';
 
+const dbaAgent = createAgent({
+  name: 'Database administrator',
+  description: 'Provides expert support for managing PostgreSQL databases',
+  system:
+    'You are a PostgreSQL expert database administrator. ' +
+    'You only provide answers to questions related to PostgreSQL database schema, indexes, and extensions.',
+  model: openai({
+    model: 'claude-sonnet-4-5-20250929',
+    baseUrl: process.env.OPENAI_API_BASE_URL,
+  }),
+});
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+   const {output} = await dbaAgent.run(
+      `Answer the question: ${event.data.value}`,
+    );
+
+    console.log("Agent output:", output);
+
+    return { success:'ok' };
   },
 );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,21 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
+
 export const appRouter = createTRPCRouter({
-  createAI: baseProcedure
+  invoke: baseProcedure
     .input(
       z.object({
-        text: z.string(),
+        value: z.string(),
       }),
     )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
+    .mutation(async ({input}) => {
+      await inngest.send({
+        name: 'test/hello.world',
+        data: {
+          value: input.value,
+        },
+      })
     }),
 });
 // export type definition of API
